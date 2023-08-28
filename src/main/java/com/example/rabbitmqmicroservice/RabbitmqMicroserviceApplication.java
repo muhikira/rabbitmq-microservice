af195088006cba0abfbe81a0e09e95bd739feda2
Muhikira <muhikiraismael@gmail.com>
2023-08-28 12:18:46 -0500

    Initial commit

---
+++ src/main/java/com/example/rabbitmqmicroservice/RabbitmqMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rabbitmqmicroservice/RabbitmqMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
