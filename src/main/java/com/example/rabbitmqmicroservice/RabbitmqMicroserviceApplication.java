aa207be3db37aec4a81170f2f32ecc49abf1af3c
Muhikira <muhikiraismael@gmail.com>
2023-08-31 13:44:10 -0500

    Last commit Event-Driven MicroSerice using spring boot and RabbitMQ

--- src/main/java/com/example/rabbitmqmicroservice/RabbitmqMicroserviceApplication.java
+++ src/main/java/com/example/rabbitmqmicroservice/RabbitmqMicroserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RabbitmqMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RabbitmqMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run (RabbitmqMicroserviceApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/rabbitmqmicroservice/RabbitmqMicroserviceApplicationTests.java
+++ src/test/java/com/example/rabbitmqmicroservice/RabbitmqMicroserviceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RabbitmqMicroserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
